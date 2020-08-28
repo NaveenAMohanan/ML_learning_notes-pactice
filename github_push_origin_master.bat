96d69892bfb6cd281a348bb92a49ff7e66c05ce6
navmoh <naveen.am@yahoo.com>
2020-08-28 22:12:02 +0530

    Updated files uploaded on `date +'m-H:S'`

--- github_push_origin_master.bat
+++ github_push_origin_master.bat
@@ -1,6 +1,6 @@
 call venv\Scripts\activate.bat
 git add .
-git commit -m "Auto upload the updated files"
+git commit -m "Updated files uploaded on `date +'%Y-%m-%d %H:%M:%S'`"
 git push origin master
 git status 
 pause
